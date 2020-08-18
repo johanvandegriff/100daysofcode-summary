51c96d9ff46ee2bb47427827e1f5519eee2d1459
Johan Vandegriff <johan@vandymail.com>
2020-08-18 04:04:32 -0400

    removed spaces and renamed vars

--- download.sh
+++ download.sh
@@ -1,22 +1,19 @@
-url="https://fosstodon.org/@johanv/tagged/100daysofcode"
-
-post_ids=""
-count=2
+u="https://fosstodon.org/@johanv/tagged/100daysofcode"
+p=""
+c=2
 i=1
-while [[ "$count" -ne 1 ]] || [[ "$i" -lt 3 ]]; do
-  page=`curl -s "$url"`
-  urls=`echo "$page" | grep "Show more" | cut -d\" -f4`
-  count=`echo "$urls" | wc -l`
-  url=`echo "$urls" | tail -1`
-  i=$((i+1))
-  post_ids="$post_ids
-`echo "$page" | grep 'https://fosstodon.org/@johanv/[0-9]'`"
+while [ "$c" -ne 1 ]||[ "$i" -lt 3 ];do
+a=`curl -s "$u"`
+s=`echo "$a"|grep "Show more"|cut -d\" -f4`
+c=`echo "$s"|wc -l`
+u=`echo "$s"|tail -1`
+i=$((i+1))
+p="$p
+`echo "$a"|grep 'https://fosstodon.org/@johanv/[0-9]'`"
 done
-
 echo '<body style="background-color:#333;color:#fff">
 <h1>#100daysofcode Day 100!</h1>'>index.html
-echo "$post_ids" | grep -v '^$' | cut -d\" -f6 | while read line; do
-  echo '<iframe src="'"$line"'/embed" class="mastodon-embed" style="max-width: 100%; border: 0" width="400" allowfullscreen="allowfullscreen"></iframe><script src="https://fosstodon.org/embed.js" async="async"></script>'
+echo "$p"|grep -v '^$'|cut -d\" -f6|while read l;do
+echo '<iframe src="'"$l"'/embed" class="mastodon-embed" style="max-width: 100%; border: 0" width="400" allowfullscreen="allowfullscreen"></iframe><script src="https://fosstodon.org/embed.js" async="async"></script>'
 done>>index.html
-
 python3 -m http.server
