58046ab9b698e86ec8ddba2209be5302fe4aedd2
Johan Vandegriff <johan@vandymail.com>
2020-08-18 04:49:42 -0400

    using   etc. for strings

--- download.sh
+++ download.sh
@@ -1,19 +1,22 @@
-u="https://fosstodon.org/@johanv/tagged/100daysofcode"
+#100daysofcode Day 100:
+#obfuscated #sh script fun :)
+set `head -1 $0|cut -c2-` johanv https://fosstodon.org/ index.html \< \> \" allowfullscreen
+u="$5@johanv/tagged/$1"
 p=""
 c=2
 i=1
 while [ "$c" -ne 1 ]||[ "$i" -lt 3 ];do
-a=`curl -s "$u"`
-s=`echo "$a"|grep "Show more"|cut -d\" -f4`
+s=`curl -s "$u"`
+p="$p
+"`echo "$s"|grep "$5@johanv/[0-9]"`
+s=`echo "$s"|grep "Show more"|cut -d\" -f4`
 c=`echo "$s"|wc -l`
 u=`echo "$s"|tail -1`
 i=$((i+1))
-p="$p
-`echo "$a"|grep 'https://fosstodon.org/@johanv/[0-9]'`"
 done
-echo '<body style="background-color:#333;color:#fff">
-<h1>#100daysofcode Day 100!</h1>'>index.html
-echo "$p"|grep -v '^$'|cut -d\" -f6|while read l;do
-echo '<iframe src="'"$l"'/embed" class="mastodon-embed" style="max-width: 100%; border: 0" width="400" allowfullscreen="allowfullscreen"></iframe><script src="https://fosstodon.org/embed.js" async="async"></script>'
-done>>index.html
+echo '<body style="background-color:#333;color:#fff">'>$6
+echo "${7}h1$8#$1 $2 $3$7/h1$8"|tr : !>>$6
+echo "$p"|grep -v ^$|cut -d$9 -f6|while read l;do
+echo "${7}iframe src=$9$l/embed$9 class=\"mastodon-embed$9 style=\"max-width: 100%; border: 0$9 width=\"400$9 $10=\"$10$9$8$7/iframe$8${7}script src=$9${5}embed.js$9 async=\"async$9$8$7/script$8"
+done>>$6
 python3 -m http.server
